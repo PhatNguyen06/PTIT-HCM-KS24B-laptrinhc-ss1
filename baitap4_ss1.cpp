7fb60192e574ff6abc75000311077fb6680b4294
PhatNguyen06 <nguyendaiphat06@gmail.com>
2024-12-07 00:08:41 +0700

    Add files via upload
    Hoàn thành baitap4_ss1

---
+++ baitap4_ss1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	float a;
+	printf("Nhap a: ");
+	scanf("%f", &a);
+	
+	if(a >= 5)
+	{
+		printf("Dat");
+	}
+	else
+	{
+		printf("Khong dat");
+	}
+	
+	return 0;
+}
